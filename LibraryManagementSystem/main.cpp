62618be101a8a6dc4fca74cee82a4394dbe7ee53
catcatcat <none>
2026-08-08 20:48:33 +0800

    Move all code into main.cpp Add all other .h and .cpp files into .gitignore

--- LibraryManagementSystem/main.cpp
+++ LibraryManagementSystem/main.cpp
@@ -1,19 +1,117 @@
 #include<iostream>
 #include<string>
-#include"menu.h"
-#include"inputListener.h"
-#include"accountHandler.h"
-#include"fileHandler.h"
-#include"bookHandler.h"
+#include<filesystem>
+#include<fstream>
+#include<conio.h>
+#include<map>
+
+namespace fs = std::filesystem;
 
 using std::cout;
+using std::cin;
 using std::endl;
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::string;
+using std::vector;
+
+std::map<int, int> keyToInt = {
+			{48, 0},
+			{49, 1},
+			{50, 2},
+			{51, 3},
+			{52, 4},
+			{53, 5},
+			{54, 6},
+			{55, 7},
+			{56, 8},
+			{57, 9}
+};
+
+struct account {
+	std::string username;
+	int permissionLevel = 0; //-1 guest, 0 user, 1 staff
+	bool exists = false;
+};
+
+class accountHandler {
+public:
+	void registerFunction();
+
+	void loginFunction();
+};
+
+class book {
+public:
+	std::string title;
+	std::string author;
+	std::string genre;
+	std::string language;
+	std::string ISBN;
+
+	int pageCount;
+	int available;
+	int borrowed;
+};
+void addBook();
+void removeBook();
+void editBook();
+void searchBook();
+void sortBook();
+void bookInterface();
+
+class fileHandler {
+public:
+	void initializeFiles();
+
+	void appendNewAccount(std::string username, std::string password);
+
+	void appendNewBook(book newBook);
+
+	account getAccountByName(std::string username);
+
+	bool validateAccount(std::string username, std::string password);
+
+	std::vector<std::string> getBookNames();
+
+
+};
+
+int listenForInt();
+
+char listenForChar();
+
+string listenForString();
+
+bool promptYesNo(std::string message);
+
+class menu {
+public:
+	void SetTitle(std::string title);
+
+	void SetDescription(std::string title);
+
+	void AppendNav(std::string navText, int overwriteIndex = -1);
+
+	void DisplayPage();
+private:
+	std::string PageTitle;
+	std::string PageDesc;
+	std::string PageNav = "";
+	int PageNavIndex = 1;
+};
 
 accountHandler AccountHandler;
 account currentUser;
 bool guest;
 
-bookHandler BookHandler;
+const fs::path CURRENT_DIRECTORY = fs::current_path();
+const fs::path ACCOUNTS_PATH = CURRENT_DIRECTORY / "accounts.txt";
+const fs::path BOOKS_PATH = CURRENT_DIRECTORY / "books.txt";
+
+const string Delimiter = ", ";
+const char BookDelimiter = '|';
 
 int main() {
 	fileHandler().initializeFiles();
@@ -33,7 +131,7 @@ int main() {
 		switch (listenForInt()) {
 			case 1:
 				cout << "entering login function";
-				AccountHandler.loginFunction(&currentUser);
+				AccountHandler.loginFunction();
 				if (currentUser.exists) {
 					cout << "yay im in!" << endl;
 				}
@@ -89,7 +187,7 @@ int main() {
 
 			case 1:
 				cout << "BROWSE";
-				BookHandler.openInterface();
+				bookInterface();
 				break;
 
 			default: continue;
@@ -99,4 +197,296 @@ int main() {
 	}
 
 	return 0;
+}
+
+bool validateAccountLogin(string username, string password) {
+	return fileHandler().validateAccount(username, password);
+}
+
+bool valueInRange(int value, int minRange, int maxRange) {
+	if (value >= minRange && value <= maxRange) {
+		return true;
+	}
+	return false;
+}
+
+bool validString(string str) { //checks if string has invalid chars (restricted to only alphanumeric chars)
+	for (int i = 0; i < str.length(); i++) {
+		int characterCode = (int)str[i];
+		// 0 - 9, A - Z, a - z
+		if (valueInRange(characterCode, 48, 57) || valueInRange(characterCode, 65, 90) || valueInRange(characterCode, 97, 122)) {
+			continue;
+		}
+		return false;
+	}
+	return true;
+}
+
+void accountHandler::registerFunction() {
+	if (!promptYesNo("Continue to registration? [y/n]\n")) {
+		return;
+	}
+	system("cls");
+
+	inputusr:
+	cout << "Input username (Alphanumeric characters only, must be between 3 to 36 characters in length.)\n"; //probably add requirements like "Must be x chars, no symbols"
+	string username = listenForString(); //add check for username conflicts
+
+	if (!validString(username)) {
+		cout << "Invalid characters.\n\n";
+		goto inputusr;
+	}
+	if (!valueInRange(static_cast<int>(username.length()), 3, 36)) {
+		cout << "Username must be between 3 to 36 characters in length.\n\n"; goto inputusr;
+	}
+
+	inputpwd:
+	cout << "Input password (Alphanumeric characters only, must be between 6 to 36 characters in length.)\n";
+	string password = listenForString();
+	cout << "Input confirm password\n";
+	string confirmPassword = listenForString();
+
+	if (!(password == confirmPassword)) {
+		cout << "Passwords do not match!\n\n"; goto inputpwd;
+	}
+	if (!validString(password)) {
+		cout << "Invalid characters.\n\n";
+		goto inputpwd;
+	}
+	if (!valueInRange(static_cast<int>(password.length()), 6, 36)) {
+		cout << "Password must be between 6 to 36 characters in length.\n\n"; goto inputpwd;
+	}
+
+	cout << "REGISTER ACCOUNT WITH USERNAME OF " << username << " AND PASSWORD OF " << password << endl;
+	fileHandler().appendNewAccount(username, password);
+}
+
+void accountHandler::loginFunction() {
+LABEL_START_LOGIN_FUNCTION:
+	if (!promptYesNo("Continue to login? [y/n]\n")) {
+		currentUser = account();
+		return;
+	}
+
+	system("cls");
+
+	cout << "Input username\n";
+	string username = listenForString();
+
+	cout << "Input password\n";
+	string password = listenForString();
+
+	cout << "LOGIN WITH USERNAME OF " << username << " AND PASSWORD OF " << password << endl;
+	if (validateAccountLogin(username, password)) {
+		cout << "LOGIN SUCCESS." << endl;
+		currentUser = fileHandler().getAccountByName(username);
+		return;
+	}
+	else {
+		cout << "INVALID USERNAME OR PASSWORD" << endl;
+		goto LABEL_START_LOGIN_FUNCTION;
+	}
+}
+
+void bookInterface() {
+	menu bookMenu;
+	bookMenu.SetTitle("Books");
+
+	LABEL_BOOK_MENU:
+	while (true) {
+		bookMenu.DisplayPage();
+		switch (listenForInt()) {
+		case 1:
+			addBook();
+			break;
+		}
+	}
+}
+
+
+void addBook() {
+	book newBook;
+
+	cout << "Enter book ISBN: ";
+	newBook.ISBN = listenForString();
+	//cin.ignore();
+	cout << "Enter book title: ";
+	newBook.title = listenForString();
+
+	cout << "Enter book category: ";
+	newBook.genre = listenForString();
+
+	cout << "Enter book author: ";
+	newBook.author = listenForString();
+
+	cout << "Enter book language: ";
+	newBook.language = listenForString();
+
+	cout << "Enter number of pages: ";
+	cin >> newBook.pageCount;
+
+	fileHandler().appendNewBook(newBook);
+
+	cout << "Book added successfully." << endl;
+	cin.ignore();
+};
+
+void fileHandler::initializeFiles() {
+	cout << "FILEHANDLER" << endl;
+	cout << "CURRENT DIRECTORY: \t" << CURRENT_DIRECTORY << endl;
+	cout << "ACCOUNTS FILEPATH: \t" << ACCOUNTS_PATH << endl;
+	cout << "BOOKS FILEPATH: \t" << BOOKS_PATH << endl;
+	cout << "Initialize Accounts file\n";
+
+	if (fs::exists(ACCOUNTS_PATH))
+		cout << "\t- accounts.txt already exists.\n";
+	else {
+		cout << "\t- accounts.txt does not exist, creating new file.\n";
+		ofstream accounts("accounts.txt");
+		accounts.close();
+	}
+
+	cout << "Initialize Books file\n";
+
+	if (fs::exists(BOOKS_PATH))
+		cout << "\t- books.txt already exists.\n";
+	else {
+		cout << "\t- books.txt does not exist, creating new file.\n";
+		ofstream books("books.txt");
+		books.close();
+	}
+}
+
+void fileHandler::appendNewAccount(string username, string password) {
+	ofstream accounts; accounts.open(ACCOUNTS_PATH, std::ios_base::app);
+	accounts << username + Delimiter + password + ", 0" + "\n";  //unsure if ", " should be used as a seperator. structure exp?: "[USR], [PWD], [PERMISSIONLVL]\n" 
+	accounts.close();
+}
+
+void fileHandler::appendNewBook(book newBook)
+{
+	ofstream booksFile;
+
+	booksFile.open(BOOKS_PATH, std::ios_base::app);
+
+	booksFile
+		<< newBook.ISBN << BookDelimiter
+		<< newBook.title << BookDelimiter
+		<< newBook.genre << BookDelimiter
+		<< newBook.author << BookDelimiter
+		<< newBook.language << BookDelimiter
+		<< newBook.pageCount
+		<< "\n";
+
+	booksFile.close();
+}
+
+vector<string> splitByDelimiter(string& str, string delimiter) {
+	vector<string> splitString;
+	size_t pos = 0;
+	string token;
+	while ((pos = str.find(delimiter)) != string::npos) {
+		token = str.substr(0, pos);
+		splitString.push_back(token);
+		str.erase(0, pos + delimiter.length());
+	}
+	splitString.push_back(str);
+	return splitString;
+}
+
+account fileHandler::getAccountByName(string username) { //name pass role
+	ifstream accounts(ACCOUNTS_PATH, std::ios::out);
+	string line;
+	while (std::getline(accounts, line)) {
+		vector<string> Row = splitByDelimiter(line, Delimiter);
+		if (Row.size() < 3) {
+			cout << "Invalid account format, skipping..." << endl << endl;
+			continue;
+		}
+		if (Row[0] == username) {
+			account givenAcc;
+			givenAcc.exists = true;
+			givenAcc.permissionLevel = stoi(Row[2]);
+			givenAcc.username = Row[0];
+			return givenAcc;
+		}
+	}
+
+	return account(); //return default exists? false
+}
+
+bool fileHandler::validateAccount(string username, string password) {
+	ifstream accounts(ACCOUNTS_PATH, std::ios::out);
+	string line;
+	while (std::getline(accounts, line)) {
+		vector<string> Row = splitByDelimiter(line, Delimiter);
+		if (Row.size() < 3) {
+			cout << "Invalid account format, skipping..." << endl << endl;
+			continue;
+		}
+		if (Row[0] == username && Row[1] == password) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+int listenForInt() {
+	while (true) {
+		char key = _getch();
+		if (keyToInt.contains((int)key)) {
+			std::cin.clear();
+			return keyToInt[key];
+		}
+	}
+}
+
+char listenForChar() {
+	return char(_getch());
+}
+
+string listenForString() {
+	string text;
+	getline(cin, text);
+	std::cin.clear();
+	return text;
+}
+
+bool promptYesNo(string message) {
+	char letter;
+	std::cout << message;
+	while (true) {
+		letter = listenForChar();
+		if (letter == 'y') {
+			std::cin.clear();
+			return true;
+		}
+		else if (letter == 'n') {
+			std::cin.clear();
+			return false;
+		}
+	}
+}
+
+void menu::SetTitle(string title) {
+	PageTitle = title;
+}
+
+void menu::SetDescription(string desc) {
+	PageDesc = desc;
+}
+
+void menu::AppendNav(string navText, int overwriteIndex) {
+	if (overwriteIndex != -1) {
+		PageNav.append("[" + std::to_string(overwriteIndex) + "] " + navText + "\n");
+		return;
+	}
+	PageNav.append("[" + std::to_string(PageNavIndex) + "] " + navText + "\n");
+	PageNavIndex++;
+}
+
+void menu::DisplayPage() {
+	system("cls");
+	cout << PageTitle << endl << PageDesc << endl << PageNav << endl;
 }
