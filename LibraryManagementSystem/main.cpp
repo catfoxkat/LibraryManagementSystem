df577d3c0efb195fba8be7ca77cd78738cb1af86
Kat <Kat@meow>
2026-08-08 14:35:09 +0800

    General changes menu.h - new SetDescription() - new arg overwriteIndex for AppendNav()
    main.cpp
    - Menu_1

--- LibraryManagementSystem/main.cpp
+++ LibraryManagementSystem/main.cpp
@@ -6,44 +6,87 @@
 #include"fileHandler.h"
 
 using std::cout;
+using std::endl;
 
 accountHandler AccountHandler;
 account currentUser;
-bool loggedIn;
+bool guest;
 
 int main() {
 	fileHandler().initializeFiles();
-	fileHandler().getAccountByName("");
+
 	std::cin.ignore();
 
-	menu NewMenu;
-	NewMenu.SetTitle("Welcome to the library of [name here]!");
-	NewMenu.AppendNav("Login");
-	NewMenu.AppendNav("Register");
-	NewMenu.AppendNav("Browse as guest");
-	NewMenu.AppendNav("Quit");
-	NewMenu.DisplayPage();
+	LABEL_START_MENU:
+	menu Menu_Main;
+	Menu_Main.SetTitle("Welcome to the library of [name here]!");
+	Menu_Main.AppendNav("Login");
+	Menu_Main.AppendNav("Register");
+	Menu_Main.AppendNav("Browse as guest");
+	Menu_Main.AppendNav("Quit");
+	Menu_Main.DisplayPage();
 
 	while (true) {
 		switch (listenForInt()) {
 			case 1:
 				cout << "entering login function";
-				AccountHandler.loginFunction();
+				AccountHandler.loginFunction(&currentUser);
+				if (currentUser.exists) {
+					cout << "yay im in!" << endl;
+				}
+				else {
+					cout << "im not in :(" << endl;
+				}
+				std::cin.ignore();
 				break;
 			case 2:
 				cout << "entering register function";
 				AccountHandler.registerFunction();
 				break;
 			case 3:
-				cout << "Not implemented :3";
+				guest = true;
+				currentUser.username = "Guest";
+				currentUser.permissionLevel = -1;
 				break;
 			case 4:
 				return 0;
 			default: continue;
 		}
 		
-		NewMenu.DisplayPage();
+		if (currentUser.exists || guest) {
+			break;
+		}
+
+		Menu_Main.DisplayPage();
 	}
 	
+	menu Menu_1;
+	Menu_1.SetTitle("Hello, " + currentUser.username + "!");
+	Menu_1.SetDescription("Permission level: " + std::to_string(currentUser.permissionLevel) + "\n");
+	
+	Menu_1.AppendNav("Browse books");
+	Menu_1.AppendNav("Facility booking");
+	Menu_1.AppendNav("Feedback");
+	if (currentUser.permissionLevel == 1) {
+		Menu_1.AppendNav("Modify books");
+		Menu_1.AppendNav("Manage users");
+		Menu_1.AppendNav("Analysis");
+	}
+	Menu_1.AppendNav("Log out", 0);
+	Menu_1.DisplayPage();
+
+	while (true) {
+		switch (listenForInt()) {
+			cout << "waiting";
+			case 0:
+				cout << "CLEARING";
+				guest = false;
+				currentUser = account();
+				goto LABEL_START_MENU;
+		}
+		cout << "left case";
+		Menu_1.DisplayPage();
+	}
+
 	return 0;
 }
--- LibraryManagementSystem/menu.h
+++ LibraryManagementSystem/menu.h
@@ -6,11 +6,14 @@ class menu {
 public:
 	void SetTitle(std::string title);
 
-	void AppendNav(std::string navText);
+	void SetDescription(std::string title);
+
+	void AppendNav(std::string navText, int overwriteIndex = -1);
 
 	void DisplayPage();
 private:
 	std::string PageTitle;
+	std::string PageDesc;
 	std::string PageNav = "";
 	int PageNavIndex = 1;
 };
